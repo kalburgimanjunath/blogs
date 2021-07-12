3c91ed7258fedd55bfc7757160c54fdc7dac54d6
manjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2021-07-12 20:00:47 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,8 @@ import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
 
 import '../styles/main.css'
-export const Box = () => (
-  `<div>
-  <div class='embedsocial-album' data-ref="6c9990cf51a1d53f22a93d4527d3f8e932327afb">
-    <a class="feed-powered-by-es" href="https://embedsocial.com/facebook-albums/" target="_blank" title="Powered by EmbedSocial">
-      </a>
-  </div>
-  <script>(function(d, s, id){var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/embedscript/ei.js"; d.getElementsByTagName("head")[0].appendChild(js);}(document, "script", "EmbedSocialScript"));</script>
-  </div>`
-  );
+import {Helmet} from "react-helmet";
+
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
@@ -30,7 +23,15 @@ export default function Nextra({ Component, pageProps }) {
         />
       </Head>
       <Component {...pageProps} />
-      <Box/>
+      <Helmet>
+                <div>
+  <div class='embedsocial-album' data-ref="6c9990cf51a1d53f22a93d4527d3f8e932327afb">
+    <a class="feed-powered-by-es" href="https://embedsocial.com/facebook-albums/" target="_blank" title="Powered by EmbedSocial">
+      </a>
+  </div>
+  <script>(function(d, s, id){var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/embedscript/ei.js"; d.getElementsByTagName("head")[0].appendChild(js);}(document, "script", "EmbedSocialScript"));</script>
+  </div>
+            </Helmet>
     </>
   )
 }
