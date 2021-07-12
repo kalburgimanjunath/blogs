c1a86fc659b9f8b47e1bdba00801923359701b40
manjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2021-07-12 21:33:16 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -42,7 +42,6 @@ export default function Nextra({ Component, pageProps }) {
         />
       </Head>
       <Component {...pageProps} />  
-      <!--<div dangerouslySetInnerHTML={{ __html: this.htmlDecode(scriptCode) }} />-->
     </>
   )
 }
